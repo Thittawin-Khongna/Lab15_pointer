5e4dbb5a784422dbe907668d08a7dc50c163954e
Thittawin <ptkk00177@gmail.com>
2025-01-31 17:59:19 +0700

    lab15_4

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -19,3 +19,14 @@ int main(){
 }
 
 //Do not modify source code above this line
+void shuffle(int &a,int &b,int &c,int &d){
+	int x[] = {a,b,c,d};
+	for(int i = 3 ; i > 0 ; i--){
+		int j = rand()%(i+1);
+		swap(x[i],x[j]);
+	}
+	a = x[0];
+	b = x[1];
+	c = x[2];
+	d = x[3];
+}
